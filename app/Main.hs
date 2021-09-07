aba9497b624ff765777b285dcfa07ec3eb2095c2
Zack Carnet <zackcarnet1@gmail.com>
2021-09-07 19:31:17 +0530

    Created actual main function

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import System.Environment
+import System.Exit
+import Lambdacalc
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do args <- getArgs
+          case args of
+            ["repl"] -> repl
+            [fn] -> do env <- nullEnv
+                       runIOExpr (eval env (Load fn)) >>= putStrLn
+            (arg:_) -> putStrLn ("Unsupported command: " ++ arg) >> exitWith (ExitFailure 1)
+            [] -> putStrLn "Use help to find usage" >> exitWith (ExitFailure 1)
